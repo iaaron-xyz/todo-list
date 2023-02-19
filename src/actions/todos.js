c75e001893ad12563806f7baef04d2dc71ac951a
Aaron <iaaron.xyz@outlook.com>
2023-02-19 15:52:54 -0600

    create appendItem function

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,33 +1,87 @@
 import { TodoItem } from '../classes/item';
 
-function saveTodoItem(e) {
-  // get current project ID
-  const projectId = Number(e.target.id.split('-').pop());
-  // Get the current todo item title
-  const todoTitleInput = document.getElementById(`todo-title-${projectId}`);
-  const todoTitle = todoTitleInput.value;
-  // Get the list of projects
-  const projectsList = JSON.parse(localStorage.getItem('projectsList'));
-  // Find the current project-object
-  projectsList.every((project) => {
-    // Create a new TodoITem and link it to the current project
-    if (project.id === projectId) {
-      // Create a new TodoItem object
-      const newItem = new TodoItem(
-        `${project.id}-${project.todoElements.length}`,
-        todoTitle,
-      );
-      // Append the new item to the current project list of todos
-      project.todoElements.push(newItem);
-      return false;
+function getProject(id) {
+  // query local storage
+  const projects = JSON.parse(localStorage.getItem('projectsList'));
+
+  // get project given an id
+  let currentProject = {};
+  projects.forEach((project) => {
+    if (id === project.id) {
+      currentProject = project;
     }
-    return true;
   });
-  // Clean input field
-  todoTitleInput.value = '';
-  // save updated list of projects
-  console.log(projectsList);
+
+  return currentProject;
+}
+
+function saveTodoItem(prId) {
+  console.log(prId);
+  // get value input
+  const todoTitleInput = document.getElementById('todo-title-input');
+  console.log(todoTitleInput.value);
+  // get the project from local storage
+  const projectsList = JSON.parse(localStorage.getItem('projectsList'));
+  projectsList.forEach((pr) => {
+    if (prId === pr.id) {
+      pr.todoElements.push(new TodoItem(
+        `${prId}-${pr.todoElements.length}`,
+        todoTitleInput.value,
+      ));
+    }
+  });
   localStorage.setItem('projectsList', JSON.stringify(projectsList));
+}
+
+function appendNewItem(prId) {
+  // get parent of todo-items
+  const todoList = document.getElementById('todo-list');
+
+  // get associated project
+  const project = getProject(prId);
+
+  console.log(project);
+
+  // get the last created item in the list
+  const latestItem = project.todoElements[project.todoElements.length - 1];
+
+  // remove no-items sign for new projects
+  if (project.todoElements.length === 1) {
+    todoList.innerHTML = '';
+  }
+
+  // Create todo item
+  const todoItem = document.createElement('div');
+  todoItem.setAttribute('class', 'project mb-2 p-4 bg-cyan-500 rounded-md w-full flex');
+  todoItem.setAttribute('id', `todo-container-${latestItem.id}`);
+  // label
+  const todoLabel = document.createElement('label');
+  todoLabel.setAttribute('class', 'todo-checkbox flex-none');
+  // input
+  const todoInput = document.createElement('input');
+  todoInput.setAttribute('type', 'checkbox');
+  todoInput.setAttribute('name', 'checkbox');
+  // paragraph
+  const todoParagraph = document.createElement('p');
+  todoParagraph.setAttribute('class', 'grow mx-2 h-8 todo-item-text');
+  todoParagraph.textContent = latestItem.title;
+  // button
+  const todoBtn = document.createElement('button');
+  todoBtn.setAttribute('class', 'self-end flex-none bg-rose-800 p-2 rounded flex');
+  todoBtn.setAttribute('id', `del-item_${latestItem.id}`);
+  todoBtn.innerHTML = `
+    <span class="material-symbols-rounded" id="del-sym_${latestItem.id}">delete</span>
+  `;
+  // eslint-disable-next-line no-use-before-define
+  todoBtn.addEventListener('click', deleteTodoItem);
+
+  todoLabel.appendChild(todoInput);
+  todoItem.appendChild(todoLabel);
+  todoItem.appendChild(todoParagraph);
+  todoItem.appendChild(todoBtn);
+
+  // appen new todo in todo-list
+  todoList.appendChild(todoItem);
 }
 
 function renderProjectDescription(project) {
@@ -48,6 +102,7 @@ function renderTodoItems(project) {
   // create basic container
   const todoItemsList = document.createElement('div');
   todoItemsList.setAttribute('class', 'todos-list text-base');
+  todoItemsList.setAttribute('id', 'todo-list');
 
   // Append todo items if any otherwise inform the user
   if (project.todoElements.length === 0) {
@@ -90,7 +145,6 @@ function renderTodoItems(project) {
     }
   }
 
-  console.log(todoItemsList);
   return todoItemsList;
 }
 
@@ -101,14 +155,12 @@ function renderAddTodoBtn(project) {
   const addTodo = document.createElement('button');
   addTodo.setAttribute('id', `add-todo-btn-${project.id}`);
   addTodo.setAttribute('class', 'project text-black bg-lime-900 mb-2 p-4 bg-cyan-500 rounded-md flex items-center w-6/12 absolute bottom-0 left-0 right-0 ml-auto mr-auto');
-  // item input value
-  const addTodoTitle = document.createElement('input');
-  addTodoTitle.setAttribute('type', 'text');
-  addTodoTitle.setAttribute('placeholder', 'Your next Task!');
-  addTodoTitle.setAttribute('class', 'rounded grow mr-4');
-  addTodoTitle.setAttribute('id', `todo-title-${project.id}`);
-  addTodoTitle.required = true;
-  // Add Todo Item btn
+  addTodo.innerHTML = `
+    <input type="text" class="rounded grow mr-4" id="todo-title-input" placeholder="My new task" required>
+  `;
+  sectionTodo.appendChild(addTodo);
+
+  // // Add Todo Item btn
   const addTodoBtn = document.createElement('button');
   addTodoBtn.setAttribute('type', 'button');
   addTodoBtn.setAttribute('id', `add-todo-btn-${project.id}`);
@@ -117,15 +169,12 @@ function renderAddTodoBtn(project) {
   // eslint-disable-next-line no-use-before-define
   addTodoBtn.addEventListener('click', createTodoItem);
 
-  addTodo.appendChild(addTodoTitle);
   addTodo.appendChild(addTodoBtn);
 
   // return addTodo;
-  sectionTodo.appendChild(addTodo);
 }
 
 function renderTodosSection(e) {
-  console.log(e.target.id);
   // Get id of the current selected project
   const currentProjectId = Number(e.target.id.split('-').pop());
   // Get todos section
@@ -140,16 +189,20 @@ function renderTodosSection(e) {
       currentProject = project;
     }
   });
-  console.log(currentProject.todoElements.length);
   contentSection.innerHTML = '';
-  contentSection.appendChild(renderProjectDescription(currentProject));
   renderAddTodoBtn(currentProject);
+  contentSection.appendChild(renderProjectDescription(currentProject));
   contentSection.appendChild(renderTodoItems(currentProject));
 }
 
 function createTodoItem(e) {
-  saveTodoItem(e);
-  renderTodosSection(e);
+  // get project id
+  const projectId = Number(e.target.id.split('-').pop());
+  saveTodoItem(projectId);
+  // get pdirect parent fo todo items
+  // const todoList = document.getElementById('todo-list');
+  // renderTodosSection(e);
+  appendNewItem(projectId);
 }
 
 function deleteTodoItem(e) {
