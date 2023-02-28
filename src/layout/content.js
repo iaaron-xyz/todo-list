cbd5880a010a3f68b4ca0622c929f91e75dacf0c
Aaron <iaaron.xyz@outlook.com>
2023-02-28 00:45:45 -0600

    style small fixes

--- src/layout/content.js
+++ src/layout/content.js
@@ -2,9 +2,9 @@ function todoListContent() {
   const todoItem = document.createElement('div');
 
   todoItem.setAttribute('id', 'todos-list-section`');
-  todoItem.setAttribute('class', 'h-full text-xl row-start-1 row-end-5');
+  todoItem.setAttribute('class', 'h-full text-xl row-start-1 row-end-5 bg-slate-200');
   todoItem.innerHTML = `
-    <div class="navbar flex bg-white p-4">
+    <div class="navbar flex bg-slate-200 p-4">
       <h2 class="grow p-3 text-purple-700 font-bold" id="navbar-title">Home</h2>
       <button class="flex-none p-3 drop-shadow-lg bg-purple-700 text-white font-bold rounded-lg" id="navbar-button-add">+ New Task</button>
     </div>
